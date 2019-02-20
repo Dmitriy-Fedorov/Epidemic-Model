446c8e5e0808b323f304d4e4568a2ef3236f4d58
Dmitriy-Fedorov <d.d.96@yandex.ru>
2019-02-20 13:52:59 -0600

    small changes

--- Raspberry_v2/helper.py
+++ Raspberry_v2/helper.py
@@ -1,10 +1,10 @@
 import os
 
 path = "A:/Dropbox/8th/Capstone/Epidemic-Model/Raspberry_v2"
-
+path = "/mnt/2C3AED6A3AED318E/Dropbox/8th/Capstone/Epidemic-Model/Raspberry_v2"
 
 nodes = ['node_1','node_2', 'node_3', 'node_4']
-files = ['pi_main.py', 'pi_node.py']
+files = ['pi_main.py', 'pi_node.py', 'pi_main_2.py']
 
 
 for node in nodes:
