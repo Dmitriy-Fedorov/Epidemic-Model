131ec2a5431d7d7d42b2fb8f752952f178d7dfcb
Dmitriy-Fedorov <d.d.96@yandex.ru>
2019-03-15 03:11:18 -0500

    v3 working unstable version

--- Raspberry_v3/helper.py
+++ Raspberry_v3/helper.py
@@ -1,12 +1,12 @@
 import os
 
-path = "A:/Dropbox/8th/Capstone/Epidemic-Model/Raspberry_v2"
-path = "/mnt/2C3AED6A3AED318E/Dropbox/8th/Capstone/Epidemic-Model/Raspberry_v2"
+path = "A:/Dropbox/8th/Capstone/Epidemic-Model/Raspberry_v3"
+path = "/mnt/2C3AED6A3AED318E/Dropbox/8th/Capstone/Epidemic-Model/Raspberry_v3"
 
-nodes = ['node_1','node_2', 'node_3', 'node_4']
-files = ['pi_main.py', 'pi_node.py', 'pi_main_2.py']
+nodes = ['node_2']
+files = ['pi_main_v3.py', 'pi_node.py']
 
 
 for node in nodes:
     for file_ in files:
-        os.link(f"{path}/{file_}", f"{path}/{node}/{file_}")  
+        os.link("{}/{}".format(path, file_), "{}/{}/{}".format(path, node, file_))  
