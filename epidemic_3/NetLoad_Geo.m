60c5194033c35c828b328429da359ad32714e20f
Dmitriy-Fedorov <d.d.96@yandex.ru>
2019-04-09 15:11:48 -0500

    function that loads adj matrix

---
+++ epidemic_3/NetLoad_Geo.m
@@ -0,0 +1,33 @@
+function Net=NetLoad_Geo(filename, N)
+
+length(A) == N
+A = csvread(filename,0,0);
+x = A(:,1);
+y = A(:,2);
+r2=r^2;
+nodes = [x; y]; % stores original coordinates
+
+l=0;
+for i=1:N
+    for j=i+1:N
+        d2=(x(i)-x(j))^2+(y(i)-y(j))^2;
+        if d2<=r2
+            l=l+1;
+            L1(l)=i;
+            L2(l)=j;
+        end;
+    end;
+end;
+
+[ NeighVec , I1 , I2 , d ] = NeighborhoodData ( N , L1 , L2 );
+I1=I1';
+I2=I2';
+Neigh{1}=NeighVec;
+
+adj=cell(1);
+adj{1}=sparse(L1,L2,1,N,N);
+adj{1}=adj{1}+adj{1}';
+
+Net={Neigh,I1,I2,d,adj,nodes};
+
+end
--- epidemic_3/main_main.m
+++ epidemic_3/main_main.m
@@ -2,22 +2,23 @@
 close all; clear all; clc;
 RunTime = 300;
 monte_rounds = 5;
-dim = [30,30]; 
+dim = [10,10]; 
 global I1a_Initial I2a_Initial N dt 
 N = dim(1)*dim(2);
 I1a_Initial = 10; %I1_A_Initial 
 I2a_Initial = 10; %I2_A_Initial 
 dt = 0.05;
+filename = '100 nodes_position.csv';
 
 global alpha mu gamma lambda kappa
-alpha = [0.03, 0.06]; % infect rate 
-mu = [0.14, 0.14]; % sleep s 
-gamma = [0.35, 0.47]; % rec rate 
+alpha = [0.1, 0.2]; % infect rate 
+mu = [0.04, 0.04]; % sleep s 
+gamma = [0.01, 0.02]; % rec rate 
 lambda = [0.05, 0.32]; % sleep I1 
 kappa = [0.04, 0.31]; % sleep I2    % [I2_s -> I2_a, I2_a -> I2_s]
 
-r0=3;
-Net2 = NetGen_Geo(N,r0,dim);
+r0=1.5;
+Net2 = NetLoad_Geo(filename, N);
 NetRnd = NetCmbn({Net2, Net2});
 [R0_rnd,R1_rnd,R2_rnd] = calc_R0(NetRnd, alpha, mu, gamma, lambda, kappa, N);
 [R1_rnd;R2_rnd]
@@ -29,36 +30,36 @@ StopCond={'RunTime', RunTime};
 
 [t, rnd_stch] = monte_new(monte_rounds,N,Para,NetRnd,x0_rnd,StopCond,I1a_Initial,I2a_Initial);
 %
-% fig = figure(1);
-% plot(t, rnd_/N)
+fig = figure(1);
+plot(t, rnd_stch(3:end,:)/N)
 %% ODE Yrys
 time_range = [0, RunTime]; 
 tic
 [t_values, rnd_ode] = ode_yrys(NetRnd, time_range);
 toc
 
-% fig = figure(2);
-% plot(t_values,compartments/N);
-% xlabel('time(t)');
-% ylabel('density');
+fig = figure(2);
+plot(t_values,rnd_ode(3:end,:)/N);
+xlabel('time(t)');
+ylabel('density');
 % % legend('i1_a','i2_a','s_a','i2_s','s_s','i1_s')  % Yrys
-% legend('Sa','Ss','Ia 1','Is 1','Ia 2','Is 2') % Dima
+legend('Ia 1','Is 1','Ia 2','Is 2') % Dima 'Sa','Ss',
 
 %% Plot Error
-fig = figure(3);
-
-plot(t_values,(rnd_ode-rnd_stch)/N);
-title('Error  abs')
-xlabel('time(t)');
-ylabel('density');
-legend('Sa','Ss','Ia 1','Is 1','Ia 2','Is 2') % Dima
-
-fig = figure(4);
-plot(t_values,(rnd_ode-rnd_stch)./rnd_ode);
-title('Error  persentage')
-xlabel('time(t)');
-ylabel('density');
-legend('Sa','Ss','Ia 1','Is 1','Ia 2','Is 2') % Dima
+% fig = figure(3);
+% 
+% plot(t_values,(rnd_ode-rnd_stch)/N);
+% title('Error  abs')
+% xlabel('time(t)');
+% ylabel('density');
+% legend('Sa','Ss','Ia 1','Is 1','Ia 2','Is 2') % Dima
+% 
+% fig = figure(4);
+% plot(t_values,(rnd_ode-rnd_stch)./rnd_ode);
+% title('Error  persentage')
+% xlabel('time(t)');
+% ylabel('density');
+% legend('Sa','Ss','Ia 1','Is 1','Ia 2','Is 2') % Dima
 %%
 % s1 = sprintf('R1_%g R2_%g ODE.xlsx', R1_rnd, R2_rnd); 
 % s2 = sprintf('R1_%g R2_%g STCH.xlsx', R1_rnd, R2_rnd); 
